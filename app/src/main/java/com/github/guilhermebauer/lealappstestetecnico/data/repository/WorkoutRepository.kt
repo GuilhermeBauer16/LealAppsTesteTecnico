5f1ad582a9a07cd80d818d5fe106cbd118fa64b7
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2025-11-29 19:15:00 -0300

    featI1: Implementation of firebase

--- app/src/main/java/com/github/guilhermebauer/lealappstestetecnico/data/repository/WorkoutRepository.kt
+++ app/src/main/java/com/github/guilhermebauer/lealappstestetecnico/data/repository/WorkoutRepository.kt
@@ -4,6 +4,9 @@ import com.github.guilhermebauer.lealappstestetecnico.data.model.Workout
 import com.github.guilhermebauer.lealappstestetecnico.utils.ResultState
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class WorkoutRepository(
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
@@ -11,15 +14,29 @@ class WorkoutRepository(
 
     private val workoutRef = db.collection("workouts")
 
-    suspend fun createWorkout(workout: Workout): ResultState<String> {
-        return try {
-            val doc = workoutRef.document()
-            val newWorkout = workout.copy(id = doc.id)
-            doc.set(newWorkout).await()
-            ResultState.Success("Workout Created")
-        } catch (e: Exception) {
-            ResultState.Error(e.message ?: "Error to create workout")
+//    suspend fun createWorkout(workout: Workout): ResultState<String> {
+//        return try {
+//            val doc = workoutRef.document()
+//            val newWorkout = workout.copy(id = doc.id)
+//            doc.set(newWorkout).await()
+//            ResultState.Success("Workout Created")
+//        } catch (e: Exception) {
+//            ResultState.Error(e.message ?: "Error to create workout")
+//        }
+//    }
+
+    suspend fun createWorkout(workout: Workout) = suspendCoroutine { continuation ->
+
+        val doc = workoutRef.document()
+        val newWorkout = workout.copy(id = doc.id)
+        doc.set(newWorkout).addOnSuccessListener {
+            continuation.resume(Unit)
+        }.addOnFailureListener {
+
+            continuation.resumeWithException(it)
         }
+
+
     }
 
     suspend fun updateWorkout(workout: Workout): ResultState<String> {
